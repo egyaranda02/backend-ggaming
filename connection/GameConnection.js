4c6752fc296c0076e00054c10eff25e27b4e9c41
egyaranda02 <egyaranda02@gmail.com>
2021-05-24 16:40:22 +0700

    change on platform TTL

--- connection/GameConnection.js
+++ connection/GameConnection.js
@@ -13,20 +13,21 @@ module.exports.getGames = async(param)=>{
     const queryData = {
     query: `PREFIX data:<http://example.com/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-    SELECT ?id ?title ?platform ?genreName ?publisher ?developer ?urlFoto
+    SELECT ?id ?title ?platformName ?genreName ?publisher ?developer ?urlFoto
     WHERE{
         ?sub rdf:type data:game
         OPTIONAL {?sub data:id ?id.}
         OPTIONAL {?sub data:title ?title.}
-        OPTIONAL {?sub data:platform ?platform.}
         OPTIONAL {?sub data:publisher ?publisher.}
         OPTIONAL {?sub data:developer ?developer.}
         OPTIONAL {?sub data:urlFoto ?urlFoto.}
+        OPTIONAL {?sub data:onPlatform ?platformID.}
+        OPTIONAL {?platformID data:platformName ?platformName.}
         OPTIONAL {?sub data:haveGenre ?genreID.}
         OPTIONAL {?genreID data:genreName ?genreName.}
         FILTER regex(?genreName, "${param.genre ? param.genre : ''}", "i")
         FILTER regex(?title, "${param.title ? param.title : ''}", "i")
-        FILTER regex(?platform, "${param.platform ? param.platform : ''}", "i")
+        FILTER regex(?platformName, "${param.platform ? param.platform : ''}", "i")
     }`
     };
     try{
@@ -40,3 +41,4 @@ module.exports.getGames = async(param)=>{
         res.status(400).json(err);
     }
 };
+
--- tools/format.js
+++ tools/format.js
@@ -2,7 +2,7 @@ module.exports = fn = data => {
     return {
         "id": data.id ? data.id.value : '',
         "title": data.title ? data.title.value : '',
-        "platform": data.platform ? data.platform.value : '',
+        "platform": data.platformName ? data.platformName.value : '',
         "genre": data.genreName ? data.genreName.value : '',
         "publisher": data.publisher ? data.publisher.value : '',
         "developer": data.developer ? data.developer.value : '',
